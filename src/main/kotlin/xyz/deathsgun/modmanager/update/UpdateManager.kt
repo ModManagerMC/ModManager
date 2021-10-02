23f4142b8688aca526615bb3f7bc4a4bc23674a9
DeathsGun <deathsgun@protonmail.com>
2021-10-02 14:43:07 +0200

    Encoding whitespaces in download urls
    Closes #70
    
    Signed-off-by: DeathsGun <deathsgun@protonmail.com>

--- src/main/kotlin/xyz/deathsgun/modmanager/update/UpdateManager.kt
+++ src/main/kotlin/xyz/deathsgun/modmanager/update/UpdateManager.kt
@@ -254,7 +254,7 @@ class UpdateManager {
                 version.assets.find { (it.filename.endsWith(".jar") || it.primary) && !it.filename.contains("forge") }
                     ?: return ModUpdateResult.Error(TranslatableText("modmanager.error.update.noFabricJar"))
             val jar = dir.resolve(asset.filename) // Download into same directory where the old jar was
-            val request = HttpRequest.newBuilder(URI.create(asset.url)).GET()
+            val request = HttpRequest.newBuilder(URI.create(encodeURI(asset.url))).GET()
                 .setHeader("User-Agent", "ModManager ${ModManager.getVersion()}").build()
             val response = this.http.send(request, HttpResponse.BodyHandlers.ofFile(jar))
             if (response.statusCode() != 200) {
@@ -277,6 +277,7 @@ class UpdateManager {
             ModManager.modManager.changed = true
             ModUpdateResult.Success
         } catch (e: Exception) {
+            e.printStackTrace()
             ModUpdateResult.Error(TranslatableText("modmanager.error.unknown.update", e))
         }
     }
@@ -405,4 +406,9 @@ class UpdateManager {
             return ModRemoveResult.Error(TranslatableText("modmanager.error.jar.failedDelete", e))
         }
     }
+
+    private fun encodeURI(url: String): String {
+        return URI("dummy", url, null).rawSchemeSpecificPart
+    }
+
 }
