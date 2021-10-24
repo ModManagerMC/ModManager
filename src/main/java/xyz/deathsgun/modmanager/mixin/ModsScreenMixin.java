4cc82b3a1cb7fa4854ea1d1ab76cc137061d0034
DeathsGun <deathsgun@protonmail.com>
2021-10-24 13:40:14 +0200

    Added textures for the buttons
    Signed-off-by: DeathsGun <deathsgun@protonmail.com>

--- src/main/java/xyz/deathsgun/modmanager/mixin/ModsScreenMixin.java
+++ src/main/java/xyz/deathsgun/modmanager/mixin/ModsScreenMixin.java
@@ -21,7 +21,6 @@ import com.terraformersmc.modmenu.gui.widget.ModMenuTexturedButtonWidget;
 import com.terraformersmc.modmenu.gui.widget.entries.ModListEntry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
@@ -33,11 +32,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.deathsgun.modmanager.ModManager;
 import xyz.deathsgun.modmanager.config.Config;
 import xyz.deathsgun.modmanager.gui.ModsOverviewScreen;
+import xyz.deathsgun.modmanager.gui.widget.TexturedButton;
 
 @Mixin(ModsScreen.class)
 public abstract class ModsScreenMixin extends Screen {
 
     private static final Identifier MODMANAGER_BUTTON_LOCATION = new Identifier("modmanager", "textures/gui/install_button.png");
+    private static final Identifier MODMANAGER_HIDE_BUTTON = new Identifier("modmanager", "textures/gui/hide_button.png");
+    private static final Identifier MODMANAGER_SHOW_BUTTON = new Identifier("modmanager", "textures/gui/show_button.png");
     @Shadow
     private int paneWidth;
     @Shadow
@@ -46,7 +48,7 @@ public abstract class ModsScreenMixin extends Screen {
     @Shadow
     private ModListEntry selected;
 
-    private ButtonWidget hideButton;
+    private TexturedButton hideButton;
 
     protected ModsScreenMixin(Text title) {
         super(title);
@@ -61,8 +63,8 @@ public abstract class ModsScreenMixin extends Screen {
         }, new TranslatableText("modmanager.button.open")));
 
         // TODO: Switch texture to a better one
-        this.hideButton = this.addDrawableChild(new ModMenuTexturedButtonWidget(width - 24 - 22, paneY, 20, 20, 0,
-                0, MODMANAGER_BUTTON_LOCATION, 32, 64, button -> {
+        this.hideButton = this.addDrawableChild(new TexturedButton(width - 24 - 22, paneY, 20, 20, 0,
+                0, MODMANAGER_HIDE_BUTTON, 32, 64, button -> {
             if (ModManager.modManager.config.getHidden().contains(selected.getMod().getId())) {
                 ModManager.modManager.config.getHidden().remove(selected.getMod().getId());
             } else {
@@ -77,6 +79,11 @@ public abstract class ModsScreenMixin extends Screen {
         // TODO: Change texture depending on status
         this.hideButton.visible = ModManager.modManager.getUpdate().getUpdates()
                 .stream().anyMatch(it -> it.getFabricId().equalsIgnoreCase(selected.mod.getId()));
+        if (ModManager.modManager.config.getHidden().contains(selected.getMod().getId())) {
+            this.hideButton.setImage(MODMANAGER_SHOW_BUTTON);
+        } else {
+            this.hideButton.setImage(MODMANAGER_HIDE_BUTTON);
+        }
     }
 
 }
---
+++ src/main/kotlin/xyz/deathsgun/modmanager/gui/widget/TexturedButton.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 DeathsGun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package xyz.deathsgun.modmanager.gui.widget
+
+import com.mojang.blaze3d.systems.RenderSystem
+import com.terraformersmc.modmenu.gui.widget.ModMenuTexturedButtonWidget
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.util.Identifier
+
+class TexturedButton(
+    x: Int,
+    y: Int,
+    width: Int,
+    height: Int,
+    private val u: Int,
+    private val v: Int,
+    texture: Identifier,
+    private val uWidth: Int,
+    private val vHeight: Int,
+    onPress: PressAction?
+) : ModMenuTexturedButtonWidget(x, y, width, height, u, v, texture, uWidth, vHeight, onPress) {
+
+    var image: Identifier = texture
+
+    override fun renderButton(matrices: MatrixStack, mouseX: Int, mouseY: Int, delta: Float) {
+        RenderSystem.setShaderColor(1f, 1f, 1f, 1f)
+        RenderSystem.setShaderTexture(0, image)
+        RenderSystem.disableDepthTest()
+        var adjustedV = v
+        if (!active) {
+            adjustedV += height * 2
+        } else if (this.isHovered) {
+            adjustedV += height
+        }
+        drawTexture(
+            matrices,
+            x, y, u.toFloat(), adjustedV.toFloat(), width, height, uWidth, vHeight
+        )
+        RenderSystem.enableDepthTest()
+        if (this.isHovered) {
+            renderTooltip(matrices, mouseX, mouseY)
+        }
+    }
+
+}
